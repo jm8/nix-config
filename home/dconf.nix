0c712729af9b2de961e027603e6ed05e33d4791a
Josh <jm8hjkl@gmail.com>
2023-08-27 15:42:16 -0400

    remove material shell

--- home/dconf.nix
+++ home/dconf.nix
@@ -1,13 +1,65 @@
 # Generated via dconf2nix: https://github.com/gvolpe/dconf2nix
-{ lib, ... }:
-
-with lib.hm.gvariant;
-
-{
+{lib, ...}:
+with lib.hm.gvariant; {
   dconf.settings = {
+    "org/gnome/Chess" = {
+      height = 881;
+      maximized = false;
+      width = 800;
+    };
+
+    "org/gnome/Console" = {
+      last-window-size = mkTuple [652 481];
+    };
+
+    "org/gnome/Geary" = {
+      migrated-config = true;
+    };
+
+    "org/gnome/Podcasts" = {
+      persist-window-geometry-height = 871;
+      persist-window-geometry-left = 5;
+      persist-window-geometry-maximized = false;
+      persist-window-geometry-top = 5;
+      persist-window-geometry-width = 1590;
+    };
+
+    "org/gnome/Totem" = {
+      active-plugins = ["vimeo" "variable-rate" "skipto" "screenshot" "screensaver" "save-file" "rotation" "recent" "movie-properties" "open-directory" "mpris" "autoload-subtitles" "apple-trailers"];
+      subtitle-encoding = "UTF-8";
+    };
+
+    "org/gnome/calendar" = {
+      active-view = "month";
+      window-maximized = true;
+      window-size = mkTuple [768 600];
+    };
+
+    "org/gnome/control-center" = {
+      last-panel = "keyboard";
+      window-state = mkTuple [839 867];
+    };
+
+    "org/gnome/desktop/app-folders" = {
+      folder-children = ["Utilities" "YaST"];
+    };
+
+    "org/gnome/desktop/app-folders/folders/Utilities" = {
+      apps = ["gnome-abrt.desktop" "gnome-system-log.desktop" "nm-connection-editor.desktop" "org.gnome.baobab.desktop" "org.gnome.Connections.desktop" "org.gnome.DejaDup.desktop" "org.gnome.Dictionary.desktop" "org.gnome.DiskUtility.desktop" "org.gnome.eog.desktop" "org.gnome.Evince.desktop" "org.gnome.FileRoller.desktop" "org.gnome.fonts.desktop" "org.gnome.seahorse.Application.desktop" "org.gnome.tweaks.desktop" "org.gnome.Usage.desktop" "vinagre.desktop"];
+      categories = ["X-GNOME-Utilities"];
+      name = "X-GNOME-Utilities.directory";
+      translate = true;
+    };
+
+    "org/gnome/desktop/app-folders/folders/YaST" = {
+      categories = ["X-SuSE-YaST"];
+      name = "suse-yast.directory";
+      translate = true;
+    };
+
     "org/gnome/desktop/input-sources" = {
-      sources = [ (mkTuple [ "xkb" "us" ]) (mkTuple [ "xkb" "gr+polytonic" ]) ];
-      xkb-options = [ "caps:escape" ];
+      sources = [(mkTuple ["xkb" "us"]) (mkTuple ["xkb" "gr+polytonic"])];
+      xkb-options = ["caps:escape"];
     };
 
     "org/gnome/desktop/interface" = {
@@ -25,11 +77,15 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/notifications" = {
-      application-children = [ "firefox" "thunar" "org-gnome-geary" "gnome-power-panel" "calibre-gui" "org-gnome-nautilus" "org-gnome-fileroller" ];
+      application-children = ["firefox" "thunar" "org-gnome-geary" "gnome-power-panel" "calibre-gui" "org-gnome-nautilus" "org-gnome-fileroller" "org-prismlauncher-prismlauncher" "code"];
     };
 
     "org/gnome/desktop/notifications/application/calibre-gui" = {
       application-id = "calibre-gui.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/code" = {
+      application-id = "code.desktop";
     };
 
     "org/gnome/desktop/notifications/application/firefox" = {
@@ -52,12 +108,28 @@ with lib.hm.gvariant;
       application-id = "org.gnome.Nautilus.desktop";
     };
 
+    "org/gnome/desktop/notifications/application/org-gnome-settings" = {
+      application-id = "org.gnome.Settings.desktop";
+    };
+
+    "org/gnome/desktop/notifications/application/org-prismlauncher-prismlauncher" = {
+      application-id = "org.prismlauncher.PrismLauncher.desktop";
+    };
+
     "org/gnome/desktop/notifications/application/thunar" = {
       application-id = "thunar.desktop";
     };
 
+    "org/gnome/desktop/peripherals/keyboard" = {
+      numlock-state = true;
+    };
+
     "org/gnome/desktop/peripherals/touchpad" = {
       two-finger-scrolling-enabled = true;
+    };
+
+    "org/gnome/desktop/search-providers" = {
+      sort-order = ["org.gnome.Contacts.desktop" "org.gnome.Documents.desktop" "org.gnome.Nautilus.desktop"];
     };
 
     "org/gnome/desktop/session" = {
@@ -65,58 +137,115 @@ with lib.hm.gvariant;
     };
 
     "org/gnome/desktop/wm/keybindings" = {
+      close = ["<Super>q"];
       cycle-panels = [];
       cycle-panels-backward = [];
-      maximize = [ "" ];
+      maximize = ["<Super>w"];
       minimize = [];
       move-to-monitor-down = [];
       move-to-monitor-left = [];
       move-to-monitor-right = [];
       move-to-monitor-up = [];
-      move-to-workspace-1 = [ "" ];
-      move-to-workspace-2 = [ "" ];
-      move-to-workspace-3 = [ "" ];
-      move-to-workspace-4 = [ "" ];
+      move-to-workspace-1 = [""];
+      move-to-workspace-2 = [""];
+      move-to-workspace-3 = [""];
+      move-to-workspace-4 = [""];
       move-to-workspace-last = [];
-      move-to-workspace-left = [];
-      move-to-workspace-right = [];
+      move-to-workspace-left = ["<Shift><Super>a"];
+      move-to-workspace-right = ["<Shift><Super>d"];
       switch-applications = [];
       switch-applications-backward = [];
-      switch-input-source = [ "" ];
-      switch-input-source-backward = [ "" ];
+      switch-input-source = [""];
+      switch-input-source-backward = [""];
       switch-panels = [];
       switch-panels-backward = [];
       switch-to-workspace-last = [];
-      switch-to-workspace-left = [];
-      switch-to-workspace-right = [];
-      unmaximize = [ "" ];
+      switch-to-workspace-left = ["<Super>a"];
+      switch-to-workspace-right = ["<Super>d"];
+      switch-windows = ["<Alt>Tab"];
+      switch-windows-backward = ["<Shift><Alt>Tab"];
+      unmaximize = [""];
     };
 
     "org/gnome/desktop/wm/preferences" = {
       auto-raise = false;
       button-layout = "appmenu:close";
       focus-mode = "sloppy";
-      num-workspaces = 1;
+      num-workspaces = 10;
+    };
+
+    "org/gnome/epiphany" = {
+      ask-for-default = false;
+    };
+
+    "org/gnome/epiphany/state" = {
+      is-maximized = true;
+      window-size = mkTuple [1600 868];
+    };
+
+    "org/gnome/evolution-data-server" = {
+      migrated = true;
+      network-monitor-gio-name = "";
+    };
+
+    "org/gnome/file-roller/dialogs/extract" = {
+      recreate-folders = true;
+      skip-newer = false;
+    };
+
+    "org/gnome/file-roller/listing" = {
+      list-mode = "as-folder";
+      name-column-width = 250;
+      show-path = false;
+      sort-method = "name";
+      sort-type = "ascending";
+    };
+
+    "org/gnome/file-roller/ui" = {
+      sidebar-width = 200;
+      window-height = 823;
+      window-width = 1570;
     };
 
     "org/gnome/mutter" = {
+      dynamic-workspaces = false;
       edge-tiling = false;
-      workspaces-only-on-primary = true;
       experimental-features = ["scale-monitor-framebuffer"];
+      workspaces-only-on-primary = true;
     };
 
     "org/gnome/mutter/keybindings" = {
-      toggle-tiled-left = [ "" ];
-      toggle-tiled-right = [ "" ];
+      toggle-tiled-left = ["<Super>z"];
+      toggle-tiled-right = ["<Super>c"];
     };
 
     "org/gnome/mutter/wayland/keybindings" = {
-      restore-shortcuts = [ "" ];
+      restore-shortcuts = [""];
+    };
+
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer = "icon-view";
+      migrated-gtk-settings = true;
+      search-filter-time-type = "last_modified";
+      search-view = "list-view";
+    };
+
+    "org/gnome/nautilus/window-state" = {
+      initial-size = mkTuple [771 814];
+      maximized = true;
+    };
+
+    "org/gnome/nm-applet" = {
+      disable-connected-notifications = true;
+    };
+
+    "org/gnome/portal/filechooser/org/prismlauncher/PrismLauncher" = {
+      last-folder-path = "/home/josh/downloads";
     };
 
     "org/gnome/settings-daemon/plugins/media-keys" = {
-      custom-keybindings = [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" ];
-      www = [ "<Shift><Super>f" ];
+      custom-keybindings = ["/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/" "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"];
+      www = ["<Shift><Super>f"];
     };
 
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
@@ -125,15 +254,21 @@ with lib.hm.gvariant;
       name = "kitty";
     };
 
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+      binding = "<Shift><Super>c";
+      command = "code --ozone-platform=wayland";
+      name = "code";
+    };
+
     "org/gnome/settings-daemon/plugins/power" = {
       power-button-action = "interactive";
       sleep-inactive-ac-type = "nothing";
     };
 
     "org/gnome/shell" = {
-      command-history = [ "gnome-terminal" "kitty" "files" "thunar" "firefox" ];
-      disabled-extensions = [];
-      enabled-extensions = [ "material-shell@papyelgringo" "unite@hardpixel.eu" ];
+      command-history = ["gnome-terminal" "kitty" "files" "thunar" "firefox"];
+      disabled-extensions = ["material-shell@papyelgringo" "unite@hardpixel.eu"];
+      enabled-extensions = [];
       welcome-dialog-last-shown-version = "42.4";
     };
 
@@ -144,21 +279,80 @@ with lib.hm.gvariant;
       taskbar-item-style = "full";
     };
 
+    "org/gnome/shell/extensions/pop-shell" = {
+      active-hint = false;
+      active-hint-border-radius = mkUint32 5;
+      mouse-cursor-focus-location = mkUint32 4;
+      show-skip-taskbar = false;
+      stacking-with-mouse = false;
+      tile-by-default = true;
+    };
+
     "org/gnome/shell/keybindings" = {
       screenshot-window = [];
       show-screen-recording-ui = [];
-      show-screenshot-ui = [ "<Shift><Super>s" ];
-      switch-to-application-1 = [ "" ];
-      switch-to-application-2 = [ "" ];
-      switch-to-application-3 = [ "" ];
-      switch-to-application-4 = [ "" ];
-      switch-to-application-5 = [ "" ];
-      switch-to-application-6 = [ "" ];
-      switch-to-application-7 = [ "" ];
-      switch-to-application-8 = [ "" ];
-      switch-to-application-9 = [ "" ];
-      toggle-application-view = [ "" ];
-      toggle-overview = [ "" ];
+      show-screenshot-ui = ["<Shift><Super>s"];
+      switch-to-application-1 = [""];
+      switch-to-application-2 = [""];
+      switch-to-application-3 = [""];
+      switch-to-application-4 = [""];
+      switch-to-application-5 = [""];
+      switch-to-application-6 = [""];
+      switch-to-application-7 = [""];
+      switch-to-application-8 = [""];
+      switch-to-application-9 = [""];
+      toggle-application-view = [""];
+      toggle-overview = [""];
+    };
+
+    "org/gnome/shell/world-clocks" = {
+      locations = "@av []";
+    };
+
+    "org/gnome/tweaks" = {
+      show-extensions-notice = false;
+    };
+
+    "org/gtk/gtk4/settings/file-chooser" = {
+      date-format = "regular";
+      location-mode = "path-bar";
+      show-hidden = true;
+      show-size-column = true;
+      show-type-column = true;
+      sidebar-width = 168;
+      sort-column = "modified";
+      sort-directories-first = false;
+      sort-order = "descending";
+      type-format = "category";
+      view-type = "list";
+      window-size = mkTuple [771 814];
+    };
+
+    "org/gtk/settings/color-chooser" = {
+      selected-color = mkTuple [true 0.9764705882352941];
+    };
+
+    "org/gtk/settings/file-chooser" = {
+      date-format = "regular";
+      location-mode = "path-bar";
+      show-hidden = false;
+      show-size-column = true;
+      show-type-column = true;
+      sidebar-width = 156;
+      sort-column = "modified";
+      sort-directories-first = false;
+      sort-order = "descending";
+      type-format = "category";
+      window-position = mkTuple [26 23];
+      window-size = mkTuple [771 424];
+    };
+
+    "org/mate/terminal/profiles/default" = {
+      background-color = "#FFFFFFFFDDDD";
+      bold-color = "#000000000000";
+      foreground-color = "#000000000000";
+      palette = "#2E2E34343636:#CCCC00000000:#4E4E9A9A0606:#C4C4A0A00000:#34346565A4A4:#757550507B7B:#060698209A9A:#D3D3D7D7CFCF:#555557575353:#EFEF29292929:#8A8AE2E23434:#FCFCE9E94F4F:#72729F9FCFCF:#ADAD7F7FA8A8:#3434E2E2E2E2:#EEEEEEEEECEC";
+      visible-name = "Default";
     };
   };
 }
